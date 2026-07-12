6040c9e34945bd556d0339268ee760bdeba4967c
Hosein Nematolahi <69923142+Hoseinnematolahi@users.noreply.github.com>
2026-07-12 18:01:32 +0330

    Update product_optimizer.py

--- engine/product_optimizer.py
+++ engine/product_optimizer.py
@@ -1,46 +1,39 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2870
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+def optimize_mix(
+    inventory,
+    products
+):
 
-\f0\fs24 \cf0 def optimize_mix(\
-    inventory,\
-    products\
-):\
-\
-\
-    result=\{\}\
-\
-\
-    for key,item in products.items():\
-\
-\
-        gram = (\
-            inventory *\
-            item["default_share"]\
-            /\
-            100\
-        )\
-\
-\
-        profit = (\
-\
-            gram *\
-            item["margin"]\
-\
-        )\
-\
-\
-        result[key]=\{\
-\
-            "gram":round(gram),\
-\
-            "profit_score":\
-            round(profit,2)\
-\
-        \}\
-\
-\
-    return result}
+
+    result={}
+
+
+    for key,item in products.items():
+
+
+        gram = (
+            inventory *
+            item["default_share"]
+            /
+            100
+        )
+
+
+        profit = (
+
+            gram *
+            item["margin"]
+
+        )
+
+
+        result[key]={
+
+            "gram":round(gram),
+
+            "profit_score":
+            round(profit,2)
+
+        }
+
+
+    return result
